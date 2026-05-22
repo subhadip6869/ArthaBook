967fdd8862038fcc02078c3e18ec6215e917d5f2
Subhadip Dutta <subhadip3105@gmail.com>
2026-05-22 20:29:09 +0530

    Adds sign-in form toggle and mobile branding
    Integrates a new `SigninForm` component, allowing users to switch between login and sign-in forms. This sets up the UI for user registration.
    
    Introduces a responsive branding section, displaying the app logo, name, and slogan on smaller screens to enhance the mobile user experience.
    
    Adjusts the authentication form container width for improved layout on mobile devices.

--- src/features/auth/components/AuthForms.tsx
+++ src/features/auth/components/AuthForms.tsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
+import Logo from "../../../assets/arthabook.png";
 import styles from "./AuthForms.module.css";
 import { LoginForm } from "./LoginForm";
+import { SigninForm } from "./SigninForm";
 
 export function AuthForms() {
 	const [isSignIn, setIsSignIn] = useState(true);
 
 	return (
-		<div className="flex lg:w-[55%] w-full items-center justify-center">
-			<div className="flex flex-col w-[60%] lg:w-[50%]">
+		<div className="flex flex-col lg:w-[55%] w-full items-center justify-center">
+			<div className="relative w-full mb-12 lg:hidden">
+				<div className="flex justify-center items-center">
+					<img src={Logo} alt="logo" width={80} height={80} />
+					<h1 className="text-3xl font-semibold text-center">
+						<span className="text-primary">Artha</span>
+						<span className="text-emerald-green">Book</span>
+					</h1>
+				</div>
+				<p className="text-sm text-center text-medium-gray/80 absolute bottom-0 w-full">
+					Your wealth. Your purpose. Your ledger.
+				</p>
+			</div>
+
+			<div className="flex flex-col w-[75%] lg:w-[50%]">
 				{/* Toggle */}
 				<div className="flex mb-5">
 					<button
@@ -25,7 +40,7 @@ export function AuthForms() {
 				</div>
 
 				{/* Forms */}
-				<LoginForm />
+				{isSignIn ? <LoginForm /> : <SigninForm />}
 			</div>
 		</div>
 	);
---
+++ src/features/auth/components/SigninForm.tsx
@@ -0,0 +1,3 @@
+export function SigninForm() {
+	return <div>SigninForm</div>;
+}
