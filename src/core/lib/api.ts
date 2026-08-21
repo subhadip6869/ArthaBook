c65fc2642ffbb58374bb7328c6f31f46aa7c20f0
Subhadip Dutta <subhadip3105@gmail.com>
2026-08-21 23:35:04 +0530

    updated api base url

--- src/core/lib/api.ts
+++ src/core/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { auth } from "../../features/auth/data/firebase/firebase";
 
 export const api = axios.create({
-	baseURL: "http://arthabook-api-nest.vercel.app",
+	baseURL: "https://arthabook-api-nest.vercel.app",
 	headers: {
 		"Content-Type": "application/json",
 	},
