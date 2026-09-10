24e5822581d04a49de1ae7fb9d0bdc4a180e738f
Subhadip Dutta <subhadip3105@gmail.com>
2026-09-10 22:57:25 +0530

    updated api endpoint with versioning

--- src/features/user/data/services/userService.ts
+++ src/features/user/data/services/userService.ts
@@ -3,11 +3,13 @@ import type { CreateUserRequest } from "../../domain/type/CreateUserRequest";
 import type { UserProfile } from "../../domain/type/User";
 
 export async function getCurrentUser(): Promise<UserProfile> {
-    const response = await api.get<UserProfile>("/users");
-    return response.data;
+  const response = await api.get<UserProfile>("/v1/profile/users");
+  return response.data;
 }
 
-export async function createUser(payload: CreateUserRequest,): Promise<UserProfile> {
-    const response = await api.post<UserProfile>("/users", payload);
-    return response.data;
+export async function createUser(
+  payload: CreateUserRequest,
+): Promise<UserProfile> {
+  const response = await api.post<UserProfile>("/v1/profile/users", payload);
+  return response.data;
 }
